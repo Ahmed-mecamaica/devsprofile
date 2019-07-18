d4e40764aed4289174f8b2efeaefc9181299d087
maika <maika@trojans-MacBook-Pro.local>
2019-07-18 15:47:28 +0200

    auto layout for iphone

---
+++ devs-profile/devs-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  devs-profile
+//
+//  Created by maika on 7/18/19.
+//  Copyright © 2019 maika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
